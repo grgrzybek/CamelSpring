cc44939a5e5daaa58ce91cdeac5644ffa3260bcc
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-12-19 13:27:18 +0100

    Fixing the example

--- src/main/java/com/main/Test.java
+++ src/main/java/com/main/Test.java
@@ -3,7 +3,6 @@ package com.main;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,7 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext(
 				"camel_spring.xml");
-		CamelContext context = new DefaultCamelContext();
+		CamelContext context = ac.getBean("test", CamelContext.class);
 
 		Exchange exchange = new DefaultExchange(context);
 		exchange.getIn().setBody("Hello");
